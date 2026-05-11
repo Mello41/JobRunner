ff5a2d74e497455b729b22194cc6bd1959d54919
Mello41 <reloadedie@yandex.ru>
2026-05-11 14:47:05 +1000

    + доп интерфейс

---
+++ JobRunner.Core/Interfaces/Execution/IJobExecutor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace JobRunner.Core.Interfaces.Execution
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface IJobExecutor
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="taskId"></param>
+        /// <param name="ct"></param>
+        /// <returns></returns>
+        Task ExecuteAsync(Guid taskId, CancellationToken ct);
+    }
+}
