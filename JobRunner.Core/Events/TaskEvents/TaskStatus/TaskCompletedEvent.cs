08279c0916e932a7a05c03063d5219c72ae671da
Mello41 <reloadedie@yandex.ru>
2026-05-14 20:38:27 +1000

    конвертирование типов DurationMs

--- JobRunner.Core/Events/TaskEvents/TaskStatus/TaskCompletedEvent.cs
+++ JobRunner.Core/Events/TaskEvents/TaskStatus/TaskCompletedEvent.cs
@@ -14,7 +14,7 @@ namespace JobRunner.Core.Events.TaskEvents.TaskStatus
         public bool Success { get; set; }
         public string? ErrorMessage { get; set; }
         public DateTime CompletionTime { get; set; }
-        public long DurationMs { get; set; }
+        public long? DurationMs { get; set; }
         public INotifySettings NotifySettings { get; set; }
     }
 }
