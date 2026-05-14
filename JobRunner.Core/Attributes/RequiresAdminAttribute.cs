c429c3ce513f8bf96728d87003172774746a628b
Mello41 <reloadedie@yandex.ru>
2026-05-14 20:40:33 +1000

    Атрибутирование (заготовка)

---
+++ JobRunner.Core/Attributes/RequiresAdminAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace JobRunner.Core.Attributes
+{
+    /// <summary>
+    /// Указание на требование прав администратора.
+    /// При генерации контроллера этот атрибут будет транслироваться в [Authorize(Roles = "Admin")]
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method |
+        AttributeTargets.Class, Inherited = true)]
+    public class RequiresAdminAttribute : Attribute
+    {
+        public string Role { get; set; } = "Admin";
+        public string? Policy { get; set; }
+    }
+}
