3237d1086a43306e79703fb946b6780527628bae
Mello41 <reloadedie@yandex.ru>
2026-06-01 12:22:20 +1000

    новый метод ITaskService -  GetStatusAsync

--- JobRunner.Core/Interfaces/EntityServices/ITaskService.cs
+++ JobRunner.Core/Interfaces/EntityServices/ITaskService.cs
@@ -21,5 +21,17 @@ namespace JobRunner.Core.Interfaces.EntityServices
         /// <remarks>Важный ньюанс - PID существует только когда задача выполняется</remarks>
         Task<T?> GetByPIDAsync(long pid, CancellationToken cancellationToken = default);
 
+        /// <summary>
+        /// Мето для получения статуса задачи (выполнена/включена/завершена с ошибкой и тд)
+        /// </summary>
+        /// <param name="guid">Идентификатор процесса (основной)</param>
+        /// <param name="pid">является необязательным (когда задача не существует)</param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        /// <remarks> так как PID существует только когда задача выполняется,
+        /// при работе этого метода он необязателен. в основном можно искать по Guid, 
+        /// однако поиск по PID будет проходить хорошо, если мы уверены, что задача запущена и не падает</remarks>
+        Task<T?> GetStatusAsync(Guid guid, long? pid, CancellationToken cancellationToken = default);
+    
     }
 }
