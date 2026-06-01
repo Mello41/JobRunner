3892f7f31347b285a6208d3af0142d1d8dd21b50
Mello41 <reloadedie@yandex.ru>
2026-06-01 12:28:56 +1000

    редактирование документации

--- JobRunner.Core/Interfaces/EntityServices/ITaskService.cs
+++ JobRunner.Core/Interfaces/EntityServices/ITaskService.cs
@@ -25,13 +25,9 @@ namespace JobRunner.Core.Interfaces.EntityServices
         /// Мето для получения статуса задачи (выполнена/включена/завершена с ошибкой и тд)
         /// </summary>
         /// <param name="guid">Идентификатор процесса (основной)</param>
-        /// <param name="pid">является необязательным (когда задача не существует)</param>
         /// <param name="cancellationToken"></param>
         /// <returns></returns>
-        /// <remarks> так как PID существует только когда задача выполняется,
-        /// при работе этого метода он необязателен. в основном можно искать по Guid, 
-        /// однако поиск по PID будет проходить хорошо, если мы уверены, что задача запущена и не падает</remarks>
-        Task<T?> GetStatusAsync(Guid guid, long? pid, CancellationToken cancellationToken = default);
+        Task<T?> GetStatusAsync(Guid guid, CancellationToken cancellationToken = default);
     
     }
 }
