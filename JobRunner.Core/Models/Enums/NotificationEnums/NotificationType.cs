98d52476c87f6d59768c40561653b6fa0e8228fe
Mello41 <reloadedie@yandex.ru>
2026-06-26 16:27:37 +1000

    разделение по типам + СМС (способы уведомлений)

--- JobRunner.Core/Models/Enums/NotificationEnums/NotificationType.cs
+++ JobRunner.Core/Models/Enums/NotificationEnums/NotificationType.cs
@@ -7,28 +7,35 @@ namespace JobRunner.Core.Models.Enums.NotificationEnums
     /// </summary>
     public enum NotificationType
     {
+        #region System
         [Display(Name = "Всплывающее окно (Desktop)")]
         Popup,
 
         [Display(Name = "Звук (Desktop)")]
         Sound,
 
-        [Display(Name = "Email (SMTP)")]
-        Email,
-
         [Display(Name = "Файловый лог (сервер)")]
         LogFile,
-
-        [Display(Name = "Windows Event Log")]
-        EventLog,
-
-        [Display(Name = "HTTP-вызов на указанный URL")]
-        Webhook,
 
         [Display(Name = "Sentry, OpenTelemetry")]
         Monitoring,
 
+        [Display(Name = "Windows Event Log")]
+        EventLog,
+        #endregion
+
+        #region userData
+        [Display(Name = "Email (SMTP)")]
+        Email,
+
+        [Display(Name = "HTTP-вызов на указанный URL")]
+        Webhook,
+
         [Display(Name = "Telegram уведомление (сообщение)")]
-        Telegram
+        Telegram,
+
+        [Display(Name = "СМС")]
+        SMS
+        #endregion
     }
 }
